b53bcea91df3150fc3fe1dab687ab6dd4bc4da5f
Extspt <w.dove17@yandex.com>
2020-11-16 11:02:54 -0800

    add email

--- node/users/user.model.js
+++ node/users/user.model.js
@@ -4,9 +4,10 @@ module.exports = model;
 
 function model(sequelize) {
     const attributes = {
-        firstName: { type: DataTypes.STRING, allowNull: false },
-        lastName: { type: DataTypes.STRING, allowNull: false },
+        firstName: { type: DataTypes.STRING, allowNull: true },
+        lastName: { type: DataTypes.STRING, allowNull: true },
         username: { type: DataTypes.STRING, allowNull: false },
+        email: { type: DataTypes.STRING, allowNull: false },
         hash: { type: DataTypes.STRING, allowNull: false },
         score: { type: DataTypes.INTEGER, allowNull: false, default:0},
         highScore: { type: DataTypes.INTEGER, allowNull: false, default:0 },
