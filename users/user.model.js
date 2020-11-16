8b7fc541483bda25a582173bac7b631a3329901f
Extspt <w.dove17@yandex.com>
2020-11-16 10:22:12 -0800

    game

---
+++ users/user.model.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = model;
+
+function model(sequelize) {
+    const attributes = {
+        firstName: { type: DataTypes.STRING, allowNull: false },
+        lastName: { type: DataTypes.STRING, allowNull: false },
+        username: { type: DataTypes.STRING, allowNull: false },
+        hash: { type: DataTypes.STRING, allowNull: false },
+        score: { type: DataTypes.INTEGER, allowNull: false },
+        highScore: { type: DataTypes.INTEGER, allowNull: false },
+        birthday: { type: DataTypes.DATE, allowNull: false },
+    };
+
+    const options = {
+        defaultScope: {
+            // exclude hash by default
+            attributes: { exclude: ['hash'] }
+        },
+        scopes: {
+            // include hash with this scope
+            withHash: { attributes: {}, }
+        }
+    };
+
+    return sequelize.define('User', attributes, options);
+}
